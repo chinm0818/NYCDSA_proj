7eb5c929a852f4ba9a278af1a16707347f8b37bc
chinm0818 <32941983+chinm0818@users.noreply.github.com>
2018-01-28 21:28:52 -0500

    nearly complete. 90% feature complete

--- projects/Nyc_Oil/server.R
+++ projects/Nyc_Oil/server.R
@@ -14,17 +14,17 @@ library(rgdal)
 library(googleVis)
 library(leaflet)
 
+
 # Establishing data points and map spatial data
 
 counties = readOGR("nycbb.shp", layer = "nycbb") #nyc boroughs map
 district = readOGR("nycc.shp", layer = "nycc")#nyc council district map
 oil1 = read.csv('oil_boilers.csv')#alldata
 oil2 = oil1 %>%
-  filter(., !is.null(Latitude))
+  filter(., !is.na(Latitude))
 dummy = read.csv('dummy.csv')#df of zero values
 # nyc_base = ggplot() + geom_polygon(data = counties, aes(x=long, y=lat, group = group)) 
 district2 <- spTransform(district, CRS("+proj=longlat +datum=WGS84")) #transform coodtype to Lat Lng
-
 
 shinyServer(function(input, output) {
   
@@ -40,7 +40,7 @@ shinyServer(function(input, output) {
   
   boro_select = reactive({
     switch(input$boro,
-           "All" = c("Manhattan", "Bronx", "Brooklyn", "Queens", "Staten Island"),
+           "All" = c("Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"),
            "Manhattan" = "Manhattan",
            "Bronx" = "Bronx",
            "Brooklyn" = "Brooklyn",
@@ -49,13 +49,7 @@ shinyServer(function(input, output) {
   })
   
   map_select = reactive({
-    switch(input$boro,
-           "All" = c(1, 2, 3, 4, 5),
-           "Manhattan" = 2,
-           "Bronx" = 5,
-           "Brooklyn" = 3,
-           "Queens" = 4,
-           "Staten Island" = 1)
+    subset(counties, boro_name %in% boro_select())
   })
   
   map_location = reactive({
@@ -68,6 +62,15 @@ shinyServer(function(input, output) {
            "Staten Island" = c(40.5795, -74.1502, 12))
   })
 
+  district_select = reactive({
+    switch(input$boro,
+           "All" = c(1:51),
+           "Manhattan" = c(1:10),
+           "Bronx" = c(11:18),
+           "Brooklyn" = c(33:48),
+           "Queens" = c(19:32),
+           "Staten Island" = c(49:51))
+  })
 
     
   filtered_oil2 = reactive({
@@ -89,27 +92,97 @@ shinyServer(function(input, output) {
   consumption = reactive ({
     filtered_oil1() %>%
     group_by(., Borough) %>%
-    summarize(., Fuel.Consumption = sum(Average.Consumption), Gas.Sales = sum(Dollars.Gas))
+    summarize(., Fuel.Consumption = sum(Average.Consumption), Gas.Sales = sum(Dollars.Gas)) %>%
+    mutate(., Fuel.Mils = Fuel.Consumption/1000000) 
+    
+  })
+  
+  consumption2 = reactive ({
+    filtered_oil1() %>%
+      group_by(., Council.District) %>%
+      summarize(., Fuel.Consumption = sum(Average.Consumption), Gas.Sales = sum(Dollars.Gas)) %>%
+      mutate(., Fuel.Thou = Fuel.Consumption/1000) 
     
   })
   
  # leaflet map with geo and data layers 
   output$map <- renderLeaflet({
     
-    leaflet(oil2) %>%
+    leaflet() %>%
       addTiles(urlTemplate = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png') %>%
-      addCircles(color = 'red') 
+      setView(40.69, -73.8, 11) %>%
+      #addCircles(color = 'red') %>%
+      addLayersControl(
+        baseGroups = c('boros', 'districts', 'points'),
+        options = layersControlOptions(collapsed = FALSE)
+      )#END layers control
     
-  })
+  })#END render leaflet(map)
   
   # putting in leaflet proxy to control map (faster?)
   
   observe({
     req(filtered_oil2()) #req func to require non empty df
+    df2 = sp::merge(counties, consumption(), by.x='boro_name', by.y='Borough')
+    df3 = sp::merge(district2, consumption2(), by.x='CounDist', by.y='Council.District')
+
+    
+    bins = c(0,4,8,12,16,Inf)
+    bins2 = c(0,250,500,1000,1500,Inf)
+    pal = colorBin('YlOrRd', domain = consumption()$Fuel.Mils, bins = bins)
+    dpal = colorBin('YlOrRd', domain = consumption2()$Fuel.Thou, bins = bins2)
+    Bpal = colorFactor(topo.colors(23), oil1$Building.Type)
+    
     leafletProxy('map', data = filtered_oil2()) %>%
       clearShapes() %>%
-      addCircles(color = 'red') %>%
-      addPolylines(data = counties[map_select(),]) %>%
+      addPolygons(data = df2[df2$boro_name %in% boro_select(),],
+                  fillColor = ~pal(Fuel.Mils),
+                  weight = 2,
+                  opacity = 0.7,
+                  fillOpacity = 0.5,
+                  color = 'gray',
+                  highlight = highlightOptions(
+                    weight = 5,
+                    color = 'white',
+                    dashArray = '',
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE),
+                  label = ~sprintf(
+                    "<strong>%s</strong><br/>%g MMBTU of fuel",
+                    boro_name, Fuel.Mils
+                  ) %>% lapply(htmltools::HTML), #label modified from example. pass data along W/~ to make sure it updates correctly
+                  labelOptions = labelOptions(
+                    style = list('font-weight' = 'normal', padding = '3px 8px'),
+                    textsize = '20px',
+                    direction = 'auto'),
+                  group = 'boros') %>%
+      addCircles(color = ~Bpal(Building.Type), 
+                 group = 'points',
+                 highlight = highlightOptions(),
+                 label = ~sprintf(
+                   "%s<br>%s<br/>%0.3g MMBTU of fuel",
+                   Facility.Address, Building.Type, Average.Consumption) %>% lapply(htmltools::HTML)) %>%
+      addPolygons(data = df3[df3$CounDist %in% district_select(),], 
+                  fillColor = ~dpal(Fuel.Thou),
+                  weight = 2,
+                  opacity = 0.7,
+                  fillOpacity = 0.5,
+                  color = 'gray',
+                  highlight = highlightOptions(
+                    weight = 5,
+                    color = 'white',
+                    dashArray = '',
+                    fillOpacity = 0.7,
+                    bringToFront = TRUE),
+                  label = ~sprintf(
+                    "<strong>%s</strong><br/>%g Thousand BTU's of fuel",
+                    CounDist, Fuel.Thou
+                  ) %>% lapply(htmltools::HTML), #label modified from example. pass data along W/~ to make sure it updates correctly
+                  labelOptions = labelOptions(
+                    style = list('font-weight' = 'normal', padding = '3px 8px'),
+                    textsize = '20px',
+                    direction = 'auto'),
+                  group = 'districts') %>%
       setView(lat = map_location()[1], lng = map_location()[2], zoom = map_location()[3])
       #addPolylines(data = district2) #leave this out for now. #later add option to toggle districts off and on
   })
@@ -151,6 +224,7 @@ shinyServer(function(input, output) {
     gvisTable(consumption())
            
   })#END  chart render
+  
 
   output$take_away = renderText({
     
